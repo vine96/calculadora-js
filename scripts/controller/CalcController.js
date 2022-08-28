0742b421dd60a9393fde65fac82c668597e99ee1
Vinícius Furtado Pecci de Lima <viniciuspecci@hotmail.com>
2022-08-28 01:15:35 -0300

    get e set

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,22 @@
 class CalcController {
-    
+    constructor(){
+        this._displayCalc = "0";
+        this._dataAtual;
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(val){
+        this._displayCalc = val;
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(date){
+        this.dataAtual = date;
+    }
 }
