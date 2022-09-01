23c17bad7288cc3f662fcef3dde3d8e798693e2a
vine96 <viniciuspecci@hotmail.com>
2022-08-31 23:49:18 -0300

    Mapeando botões

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -6,6 +6,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize(){
@@ -15,6 +16,16 @@ class CalcController {
         setInterval(()=>{
             this.setDisplayDateTime();
         }, 1000);
+    }
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index)=>{
+            btn.addEventListener('click', e => {
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });
+        });
     }
 
     setDisplayDateTime(){
